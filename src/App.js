3f02c1fbdcc1187888e9b4667b1ff47d43208572
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-08-17 11:51:00 -0300

    tentando fazer o tratamento das requisicoes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import {useState, useEffect} from 'react'
+
+const url = "http://localhost:3000/products"
+
 function App() {
+  const [produtos, setProdutos] = useState([])
+
+  //resgatando dados
+  useEffect(async() => {
+  
+    const response = await fetch(url)
+
+    const data = await response.json()
+
+    setProdutos(data)
+
+  }, [])
+
+  console.log(produtos)
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
